c67ef1201f8939ff9e047e9edd56c3c00952360a
KUNJAPPAN <69103277+hopepsycho@users.noreply.github.com>
2022-04-26 13:17:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/hopepsycho/locked.git /locked
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /locked
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
-echo "Starting Ajax....🔥"
+echo "Starting locked....🔥"
 python3 bot.py
