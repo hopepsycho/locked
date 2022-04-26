790a40fa0cbb065fa98f10a1b6580b5f2e774b28
KUNJAPPAN <69103277+hopepsycho@users.noreply.github.com>
2022-04-26 11:23:57 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -60,7 +60,7 @@ async def showid(client, message):
 @Client.on_message(filters.command("about"))
 async def aboutme(client, message):
         buttons= [[
-            InlineKeyboardButton('♥️ UPDATES CHANNEL ♥️', url='https://t.me/TrockersUpdates')
+            InlineKeyboardButton('♥️ UPDATES CHANNEL ♥️', url='https://t.me/kunjappansupportez')
             ],[
             InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start'),
             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 🔐', callback_data='close_data')
