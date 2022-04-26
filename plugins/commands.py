df48848b2a795c63a8e6e706057ec1d1f3196cf1
KUNJAPPAN <69103277+hopepsycho@users.noreply.github.com>
2022-04-26 14:07:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -32,7 +32,7 @@ async def start(client, message):
             InlineKeyboardButton('𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿', url=f"https://t.me/kunjappansupport")
             ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(script.PRIVATEBOT_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
+        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/hopepsycho/locked/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
